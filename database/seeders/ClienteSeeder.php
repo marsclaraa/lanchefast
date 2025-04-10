5667d1269e918ac11494a42c6416810f207fbcdf
marsclaraa <mariaclaralopesdemaria@gmail.com>
2025-04-10 12:37:07 -0300

    Incrementado: Model, Migration, Livewire Index, Blade e Rota do Index do Cliente.

---
+++ database/seeders/ClienteSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cliente;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClienteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cliente::create([
+            'nome' => 'Cliente Exemplo',
+            'endereco' => 'Rua Exemplo, 123',
+            'telefone' => '11999999999',
+            'cpf' => '12345678901',
+            'email' => 'cliente@example.com',
+            'senha' => bcrypt('senha123')
+        ]);
+        Cliente::create([
+            'nome' => 'Cliente Exemplo',
+            'endereco' => 'Rua Exemplo, 123',
+            'telefone' => '11999999999',
+            'cpf' => '12345678902',
+            'email' => 'cliente2@example.com',
+            'senha' => bcrypt('senha123')
+
+        ]);
+        Cliente::create([
+            'nome' => 'Cliente Exemplo',
+            'endereco' => 'Rua Exemplo, 123',
+            'telefone' => '11999999999',
+            'cpf' => '12345678903',
+            'email' => 'cliente3@example.com',
+            'senha' => bcrypt('senha123')
+
+        ]);
+    }
+}
